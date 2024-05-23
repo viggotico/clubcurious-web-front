eddaa72f2f3fc8d92840b3776c8ba824f733ef73
viggotico <victors4ngo@gmail.com>
2024-05-23 11:16:50 +0200

    make assets folder

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import bgTextureUrl from '../../assets/bg-texture.png';
 import MainLayout from './MainLayout';
 import Home from './Home';
 import Events from './Events';
@@ -8,6 +10,10 @@ import Contact from './Contact';
 import NotFound from './NotFound';
 
 const Pages = ({ }) => {
+    useEffect(() => {
+        document.getElementById('bg-texture').style.backgroundImage = `url("${bgTextureUrl}")`;
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
