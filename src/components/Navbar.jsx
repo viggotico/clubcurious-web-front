f3eaf41d8667ac5a0386ad54b49a01d44e4cf519
viggotico <victors4ngo@gmail.com>
2024-05-24 03:38:33 +0200

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,21 @@ import useTranslation from '../hooks/useTranslation';
 
 // Komponent som tjekker om nav elementet skal være aktiv (rød farve)
 // for at vise at det er den nuværende side man er på
-const MenuLink = ({ children, to }) => {
+const MenuLink = ({ children, to, mobileNavVisible }) => {
+    const [mobileNavVisibleBusy, setMobileNavVisibleBusy] = useState(false);
     const className = to.includes('#') ? '' : style.active; // tjekker om linket til nav elementet er en id (hashtag)
+    const handleMobileNav = (e) => {
+        if (!mobileNavVisible) return;
+        if (mobileNavVisibleBusy) return;
+        setMobileNavVisibleBusy(true);
+        const dropdown = e.target.closest('li');
+        console.log("dropdown.children[1]:", dropdown.children[1].classList);
+        dropdown.children[1].classList.toggle(style.show);
+        setTimeout(() => setMobileNavVisibleBusy(false), 200);
+    };
     return (
         <NavLink
+            onClick={handleMobileNav} onTouchStart={handleMobileNav}
             to={to} // Nav elementets URL
             className={({ isActive }) => isActive ? className : ''} // tjekker om nav elementet er aktivt
         >
@@ -24,10 +35,10 @@ const MenuLink = ({ children, to }) => {
 
 // Komponent til dropdown menuer så man ikke skal skrive HTML'et flere gange
 // da HTML'et er ens for alle dropdown menuer
-const Dropdown = ({ children, to, name }) => {
+const Dropdown = ({ children, to, name, mobileNavVisible }) => {
     return (
         <li className={style.dropdown}>
-            <MenuLink to={to}>
+            <MenuLink to={to} mobileNavVisible={mobileNavVisible}>
                 {name}
                 <div className={style.iconWrapper}>
                     {/* 'ExpandCircleDownIcon' er en komponent til SVG ikonet til dropdown menuen */}
@@ -47,6 +58,7 @@ const Dropdown = ({ children, to, name }) => {
 const Navbar = ({ }) => {
     const translation = useTranslation();
     const [mobileNavVisible, setMobileNavVisible] = useState(false);
+    const [mobileNavVisibleBusy, setMobileNavVisibleBusy] = useState(false);
 
     const navItemName = (id = "home") => translation.get('navbar', id);
 
@@ -56,10 +68,13 @@ const Navbar = ({ }) => {
     }
 
     const toggleMobileNav = (e) => {
+        if (mobileNavVisibleBusy) return;
+        setMobileNavVisibleBusy(true);
         const burgerIcon = e.target.closest('div');
-        e.preventDefault();
-        burgerIcon.classList.toggle(style.activeNavMobile);
+        if (mobileNavVisible) burgerIcon.classList.remove(style.activeNavMobile);
+        else burgerIcon.classList.add(style.activeNavMobile);
         setMobileNavVisible(burgerIcon.classList.contains(style.activeNavMobile));
+        setTimeout(() => setMobileNavVisibleBusy(false), 200);
     }
 
     return (
@@ -74,14 +89,14 @@ const Navbar = ({ }) => {
                         </div>
                         <ul className={style.menu}>
                             <li><MenuLink to='/' >{navItemName('home')}</MenuLink></li>
-                            <Dropdown to='/events' name={navItemName('events')}>
+                            <Dropdown mobileNavVisible={mobileNavVisible} to='/events' name={navItemName('events')}>
                                 <li><MenuLink to='/events#billetto'>{navItemName('calender')}</MenuLink></li>
                             </Dropdown>
-                            <Dropdown to='/collab' name={navItemName('collab')}>
+                            <Dropdown mobileNavVisible={mobileNavVisible} to='/collab' name={navItemName('collab')}>
                                 <li><MenuLink to='/collab#community'>{navItemName('community')}</MenuLink></li>
                                 <li><MenuLink to='/collab#sponsor'>{navItemName('sponsor')}</MenuLink></li>
                             </Dropdown>
-                            <Dropdown to='/about' name={navItemName('about')}>
+                            <Dropdown mobileNavVisible={mobileNavVisible} to='/about' name={navItemName('about')}>
                                 <li><MenuLink to='/about#hours'>{navItemName('hours')}</MenuLink></li>
                                 <li><MenuLink to='/about#gallery'>{navItemName('gallery')}</MenuLink></li>
                                 <li><MenuLink to='/about#address'>{navItemName('address')}</MenuLink></li>
@@ -103,14 +118,14 @@ const Navbar = ({ }) => {
                         </div>
                         <ul className={`${style.menu}${mobileNavVisible ? ` ${style.showNavMobile}` : ''}`}>
                             <li><MenuLink to='/' >{navItemName('home')}</MenuLink></li>
-                            <Dropdown to='/events' name={navItemName('events')}>
+                            <Dropdown mobileNavVisible={mobileNavVisible} to='/events' name={navItemName('events')}>
                                 <li><MenuLink to='/events#billetto'>{navItemName('calender')}</MenuLink></li>
                             </Dropdown>
-                            <Dropdown to='/collab' name={navItemName('collab')}>
+                            <Dropdown mobileNavVisible={mobileNavVisible} to='/collab' name={navItemName('collab')}>
                                 <li><MenuLink to='/collab#community'>{navItemName('community')}</MenuLink></li>
                                 <li><MenuLink to='/collab#sponsor'>{navItemName('sponsor')}</MenuLink></li>
                             </Dropdown>
-                            <Dropdown to='/about' name={navItemName('about')}>
+                            <Dropdown mobileNavVisible={mobileNavVisible} to='/about' name={navItemName('about')}>
                                 <li><MenuLink to='/about#hours'>{navItemName('hours')}</MenuLink></li>
                                 <li><MenuLink to='/about#gallery'>{navItemName('gallery')}</MenuLink></li>
                                 <li><MenuLink to='/about#address'>{navItemName('address')}</MenuLink></li>
@@ -118,6 +133,7 @@ const Navbar = ({ }) => {
                             <li><MenuLink to='/contact'>{navItemName('contact')}</MenuLink></li>
                         </ul>
                     </div>
+                    <li className={style.languageMobile} onClick={changeLangage}><span>{translation.language}</span><LanguageIcon /></li>
                     <div className={style.burgerIcon} onClick={toggleMobileNav} onTouchStart={toggleMobileNav}>
                         <div></div>
                         <div></div>
